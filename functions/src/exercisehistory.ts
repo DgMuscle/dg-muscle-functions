af88a6057339bb8e3294fc59aca7f2140ef7eddc
신동규 <donggyu9410@gmail.com>
2023-09-29 21:51:14 +0900

    style

--- functions/src/exercisehistory.ts
+++ functions/src/exercisehistory.ts
@@ -15,62 +15,67 @@ interface Record {
 }
 
 export const getHistories = onRequest(async (req, res) => {
-    const uid = req.get("uid");
-    const lastId = req.query.lastId;
+  const uid = req.get("uid");
+  const lastId = req.query.lastId;
 
-    if (typeof uid == "undefined") {
-        res.json({
-            ok: false,
-            message: "authentication error",
-        });
-    }
-
-    let ref = db.collection("users").doc(uid ?? "").collection("histories").orderBy("date", "desc");
-
-    if (typeof lastId == "string") {
-        const previousSnapshot = await db.collection("users").doc(uid ?? "").collection("histories").doc(lastId).get();
-        ref = ref.startAfter(previousSnapshot);
-    }
-
-    const snapshot = await ref.limit(100).get();
-
-    const data = snapshot.docs.map((doc) => doc.data());
-
+  if (typeof uid == "undefined") {
     res.json({
-        data
+      ok: false,
+      message: "authentication error",
     });
+  }
+
+  let ref = db.collection("users").doc(uid ?? "")
+    .collection("histories")
+    .orderBy("date", "desc");
+
+  if (typeof lastId == "string") {
+    const previousSnapshot = await db.collection("users")
+      .doc(uid ?? "")
+      .collection("histories")
+      .doc(lastId).get();
+    ref = ref.startAfter(previousSnapshot);
+  }
+
+  const snapshot = await ref.limit(100).get();
+
+  const data = snapshot.docs.map((doc) => doc.data());
+
+  res.json({
+    data,
+  });
 });
 
 export const postHistory = onRequest(async (req, res) => {
-    const uid = req.get("uid");
-    const id: string = req.body["id"];
-    const date: string = req.body["date"];
-    const records: Record[] = req.body["records"] ?? [];
+  const uid = req.get("uid");
+  const id: string = req.body["id"];
+  const date: string = req.body["date"];
+  const records: Record[] = req.body["records"] ?? [];
 
-    if (typeof uid == "undefined") {
-        res.json({
-            ok: false,
-            message: "authentication error",
-        });
-    }
-
-    if (id == null || date == null || records.length == 0) {
-        res.json({
-            ok: false,
-            message: "wrong parameter",
-        });
-    }
-
-    const data = {
-        id,
-        date,
-        records,
-        createdAt: FieldValue.serverTimestamp(),
-    };
-
-    await db.collection(`users/${uid}/histories`).doc(id).set(data);
-
+  if (typeof uid == "undefined") {
     res.json({
-        ok: true,
+      ok: false,
+      message: "authentication error",
     });
+  }
+
+  if (id == null || date == null || records.length == 0) {
+    res.json({
+      ok: false,
+      message: "wrong parameter",
+    });
+  }
+
+  const data = {
+    id,
+    date,
+    records,
+    createdAt: FieldValue.serverTimestamp(),
+  };
+
+  await db.collection(`users/${uid}/histories`).doc(id).set(data);
+
+  res.json({
+    ok: true,
+  });
 });
