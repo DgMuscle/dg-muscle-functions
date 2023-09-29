7e56216c1951521bb122a84408c51757d42976b1
신동규 <donggyu9410@gmail.com>
2023-09-29 11:54:36 +0900

    post exercise

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,6 +9,12 @@
 
 import {onRequest} from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
+import {initializeApp} from "firebase-admin/app";
+import {getFirestore, FieldValue} from "firebase-admin/firestore";
+
+initializeApp();
+
+const db = getFirestore();
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
@@ -16,4 +22,47 @@ import * as logger from "firebase-functions/logger";
 export const helloWorld = onRequest((request, response) => {
   logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
+});
+
+export const postExercise = onRequest(async (req, res) => {
+  const uid = req.get("uid");
+  const exercise_id: string = req.body["id"];
+  const exercise_name: string = req.body["name"];
+  const exercise_part: string = req.body["part"];
+  const exercise_order: number = req.body["order"];
+  const favorite: boolean = req.body["favorite"] ?? false;
+
+  if (exercise_id == null || exercise_name == null || exercise_order == null) {
+    res.json({
+      ok: false,
+      message: "exercise requires id, name, order"
+    })
+  }
+
+  if (typeof(exercise_order) != "number") {
+    res.json({
+      ok: false,
+      message: "exercise order must be integer number"
+    })
+  }
+
+  if (typeof(favorite) != "boolean") {
+    res.json({
+      ok: false, 
+      message: "favorite must be boolean"
+    })
+  }
+
+  const data = {
+    name: exercise_name,
+    part: exercise_part,
+    order: exercise_order,
+    favorite,
+    created_at: FieldValue.serverTimestamp()
+  };
+
+  await db.collection(`users/${uid}/exercises`).doc(exercise_id).set(data);
+  res.json({
+    ok: true
+  });
 });
