b34357079cd54c55c46b5524108be0d401563c47
신동규 <donggyu9410@gmail.com>
2023-09-29 12:06:37 +0900

    clean code

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,19 +6,10 @@
  *
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
-
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
 import {initializeApp} from "firebase-admin/app";
 
 initializeApp();
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
-
-export const helloWorld = onRequest((request, response) => {
-  logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
-
 exports.postExercise = require("./exercise");
