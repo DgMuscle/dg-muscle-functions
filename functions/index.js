7956c14c67167cc21d9548e0028fb67936526b7a
신동규 <donggyu9410@gmail.com>
2024-04-24 03:00:51 +0900

    asd

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,19 @@
-// The Cloud Functions for Firebase SDK to create Cloud Functions and triggers.
-const {logger} = require("firebase-functions");
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * const {onCall} = require("firebase-functions/v2/https");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
 const {onRequest} = require("firebase-functions/v2/https");
-const {onDocumentCreated} = require("firebase-functions/v2/firestore");
+const logger = require("firebase-functions/logger");
 
-// The Firebase Admin SDK to access Firestore.
-const {initializeApp} = require("firebase-admin/app");
-const {getFirestore} = require("firebase-admin/firestore");
+// Create and deploy your first functions
+// https://firebase.google.com/docs/functions/get-started
 
-initializeApp();
-
-exports.addmessage = onRequest(async (req, res) => {
-  res.json({result: "hello world"});
+exports.helloWorld = onRequest((request, response) => {
+  logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
 });
