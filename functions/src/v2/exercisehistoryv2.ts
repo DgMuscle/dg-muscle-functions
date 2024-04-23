aa67bc1eda3ea3919e06d2cb2f87265920cd02ca
신동규 <donggyu9410@gmail.com>
2024-04-23 23:44:13 +0900

    asd

---
+++ functions/src/v2/exercisehistoryv2.ts
@@ -0,0 +1,121 @@
+import {onRequest} from "firebase-functions/v2/https";
+import {getFirestore, FieldValue} from "firebase-admin/firestore";
+
+const db = getFirestore();
+
+interface Set {
+    weight: number;
+    unit: string;
+    reps: number;
+}
+
+interface Record {
+    exerciseId: string;
+    sets: [Set];
+}
+
+export const delete_history = onRequest(async (req, res) => {
+  const uid = req.get("uid");
+  const id = req.body.id;
+
+  if (uid == undefined) {
+    res.json({
+      ok: false,
+      message: "authentication error",
+    });
+  }
+
+  if (id == undefined) {
+    res.json({
+      ok: false,
+      message: "id is required",
+    });
+  }
+
+  await db.collection("users")
+    .doc(uid ?? "")
+    .collection("histories")
+    .doc(id)
+    .delete();
+
+  res.json({
+    ok: true,
+  });
+});
+
+export const get_histories = onRequest(async (req, res) => {
+  const uid = req.get("uid");
+  const lastId = req.query.lastId;
+  const limit = req.query.limit ?? "365";
+
+  if (typeof uid == "undefined") {
+    res.json({
+      ok: false,
+      message: "authentication error",
+    });
+  }
+
+  let ref = db.collection("users").doc(uid ?? "")
+    .collection("histories")
+    .orderBy("date", "desc");
+
+  if (typeof lastId == "string") {
+    const previousSnapshot = await db.collection("users")
+      .doc(uid ?? "")
+      .collection("histories")
+      .doc(lastId).get();
+    ref = ref.startAfter(previousSnapshot);
+  }
+
+  const snapshot = await ref.limit(Number(limit)).get();
+  const data = snapshot.docs.map((doc) => doc.data());
+
+  res.json(data);
+});
+
+export const post_history = onRequest(async (req, res) => {
+  const uid = req.get("uid");
+  const id: string = req.body["id"];
+  const date: string = req.body["date"];
+  const records: Record[] = req.body["records"] ?? [];
+  const memo: string | undefined = req.body["memo"];
+
+  if (typeof uid == "undefined") {
+    res.json({
+      ok: false,
+      message: "authentication error",
+    });
+  }
+
+  if (id == null || date == null || records.length == 0) {
+    res.json({
+      ok: false,
+      message: "wrong parameter",
+    });
+  }
+
+  interface Data {
+    id: string;
+    date: string;
+    records: Record[];
+    createdAt: FieldValue;
+    memo?: string;
+  }
+
+  const data: Data = {
+    id,
+    date,
+    records,
+    createdAt: FieldValue.serverTimestamp(),
+  };
+
+  if (memo != undefined) {
+    data.memo = memo;
+  }
+
+  await db.collection(`users/${uid}/histories`).doc(id).set(data);
+
+  res.json({
+    ok: true,
+  });
+});
---
+++ functions/src/v2/exercisev2.ts
@@ -0,0 +1,203 @@
+import {onRequest} from "firebase-functions/v2/https";
+import {getFirestore, FieldValue} from "firebase-admin/firestore";
+
+const db = getFirestore();
+
+export const delete_exercise = onRequest(async (req, res) => {
+  const uid = req.get("uid");
+  const id: string = req.body["id"];
+
+  if (typeof uid == "undefined") {
+    res.json({
+      ok: false,
+      message: "authentication error",
+    });
+  }
+
+  if (id == null) {
+    res.json({
+      ok: false,
+      message: "id is required",
+    });
+  }
+
+  await db.collection("users")
+    .doc(uid ?? "")
+    .collection("exercises")
+    .doc(id)
+    .delete();
+
+  res.json({
+    ok: true,
+  });
+});
+
+export const get_exercises = onRequest(async (req, res) => {
+  const uid = req.get("uid");
+
+  if (typeof uid == "undefined") {
+    res.json({
+      ok: false,
+      message: "authentication error",
+    });
+  }
+
+  const ref = db.collection("users").doc(uid ?? "").collection("exercises");
+  const snapshot = await ref.get();
+  const data = snapshot.docs.map((doc) => doc.data());
+
+  res.json(data);
+});
+
+// Override exercise datas by exercises of req.body
+export const set_exercises = onRequest(async (req, res) => {
+  interface Exercise {
+    id: string;
+    name: string;
+    parts: string[];
+    favorite: boolean;
+    order: number;
+    createdAt?: FieldValue;
+  }
+
+  const uid = req.get("uid");
+  let exercises: Exercise[] = req.body;
+
+  if (typeof uid == "undefined") {
+    res.json({
+      ok: false,
+      message: "authentication error",
+    });
+  }
+
+  exercises = exercises.map((exercise) => {
+    if (typeof exercise.createdAt == "undefined") {
+      exercise.createdAt = FieldValue.serverTimestamp();
+    }
+    return exercise;
+  });
+
+  await deleteCollection(db, `users/${uid}/exercises`);
+
+  const promises = exercises.map((exercise) => {
+    db.collection(`users/${uid}/exercises`).doc(exercise.id).set(exercise);
+  });
+
+  await Promise.all(promises);
+
+  res.json({
+    ok: true,
+  });
+});
+
+/**
+ * Represents a deleteCollection.
+ * @constructor
+ * @param {FirebaseFirestore.Firestore} db - The db of firestore.
+ * @param {string} collectionPath - The path of collection to remove
+ */
+async function deleteCollection(
+  db: FirebaseFirestore.Firestore,
+  collectionPath: string
+) {
+  const collectionRef = db.collection(collectionPath);
+  const query = collectionRef;
+
+  return new Promise((resolve, reject) => {
+    deleteQueryBatch(db, query, resolve).catch(reject);
+  });
+}
+
+/**
+ * @callback resolve
+ * @param  {unknown?} value   - Index of array element
+ */
+
+/**
+ * Represents a deleteQueryBatch.
+ * @constructor
+ * @param {FirebaseFirestore.Firestore} db - The db of firestore.
+ * @param {
+ * FirebaseFirestore.Query<FirebaseFirestore.DocumentData>
+ * } query - The query to conduct.
+ * @param {resolve} resolve - The resolve function.
+ * @return {void} - Return of resulve callback.
+ */
+async function deleteQueryBatch(
+  db: FirebaseFirestore.Firestore,
+  query: FirebaseFirestore.Query<FirebaseFirestore.DocumentData>,
+  resolve: (value?: unknown) => void
+) {
+  const snapshot = await query.get();
+
+  const batchSize = snapshot.size;
+  if (batchSize === 0) {
+    // When there are no documents left, we are done
+    resolve();
+    return;
+  }
+
+  // Delete documents in a batch
+  const batch = db.batch();
+  snapshot.docs.forEach((doc) => {
+    batch.delete(doc.ref);
+  });
+  await batch.commit();
+
+  // Recurse on the next process tick, to avoid
+  // exploding the stack.
+  process.nextTick(() => {
+    deleteQueryBatch(db, query, resolve);
+  });
+}
+
+export const post_exercise = onRequest(async (req, res) => {
+  const uid = req.get("uid");
+  const exerciseId: string = req.body["id"];
+  const exerciseName: string = req.body["name"];
+  const exerciseParts: string[] = req.body["parts"] ?? [];
+  const exerciseOrder: number = req.body["order"];
+  const favorite: boolean = req.body["favorite"] ?? false;
+
+  if (typeof uid == "undefined") {
+    res.json({
+      ok: false,
+      message: "authentication error",
+    });
+  }
+
+  if (exerciseId == null || exerciseName == null || exerciseOrder == null) {
+    res.json({
+      ok: false,
+      message: "exercise requires id, name, order",
+    });
+  }
+
+  if (typeof(exerciseOrder) != "number") {
+    res.json({
+      ok: false,
+      message: "exercise order must be integer number",
+    });
+  }
+
+  if (typeof(favorite) != "boolean") {
+    res.json({
+      ok: false,
+      message: "favorite must be boolean",
+    });
+  }
+
+  const data = {
+    id: exerciseId,
+    name: exerciseName,
+    parts: exerciseParts,
+    order: exerciseOrder,
+    favorite,
+    createdAt: FieldValue.serverTimestamp(),
+  };
+
+  await db.collection(`users/${uid}/exercises`).doc(exerciseId).set(data);
+  res.json({
+    ok: true,
+  });
+});
