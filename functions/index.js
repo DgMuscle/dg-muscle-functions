4a818e74ba86eeb7764ed622e95e10bfffb3a1d5
신동규 <donggyu9410@gmail.com>
2024-04-24 10:02:55 +0900

    chore

--- functions/index.js
+++ functions/index.js
@@ -6,9 +6,6 @@
  *
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
-
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
 const {initializeApp} = require("firebase-admin/app");
 
 // Create and deploy your first functions
