237be5e0cd7d10f59c59527da8eab1d852913c65
신동규 <donggyu9410@gmail.com>
2024-04-24 10:00:24 +0900

    history

---
+++ functions/api/history.js
@@ -0,0 +1,102 @@
+const {getFirestore, FieldValue} = require("firebase-admin/firestore");
+const functions = require('firebase-functions');
+
+const db = getFirestore();
+
+exports.deletehistory = functions.https.onRequest(async (req, res) => {
+  const uid = req.get("uid");
+  const id = req.body.id;
+
+  if (uid == undefined) {
+    res.json({
+      ok: false,
+      message: "authentication error",
+    });
+  }
+
+  if (id == undefined) {
+    res.json({
+      ok: false,
+      message: "id is required",
+    });
+  }
+
+  await db.collection("users")
+    .doc(uid ?? "")
+    .collection("histories")
+    .doc(id)
+    .delete();
+
+  res.json({
+    ok: true,
+  });
+});
+
+exports.gethistories = functions.https.onRequest(async (req, res) => {
+  const uid = req.get("uid");
+  const lastId = req.query.lastId;
+  const limit = req.query.limit ?? "365";
+
+  if (typeof uid == "undefined") {
+    res.json({
+      ok: false,
+      message: "authentication error",
+    });
+  }
+
+  let ref = db.collection("users").doc(uid ?? "")
+    .collection("histories")
+    .orderBy("date", "desc");
+
+  if (typeof lastId == "string") {
+    const previousSnapshot = await db.collection("users")
+      .doc(uid ?? "")
+      .collection("histories")
+      .doc(lastId).get();
+    ref = ref.startAfter(previousSnapshot);
+  }
+
+  const snapshot = await ref.limit(Number(limit)).get();
+  const data = snapshot.docs.map((doc) => doc.data());
+
+  res.json(data);
+});
+
+exports.posthistory = functions.https.onRequest(async (req, res) => {
+  const uid = req.get("uid");
+  const id = req.body["id"];
+  const date = req.body["date"];
+  const records = req.body["records"] ?? [];
+  const memo = req.body["memo"];
+
+  if (typeof uid == "undefined") {
+    res.json({
+      ok: false,
+      message: "authentication error",
+    });
+  }
+
+  if (id == null || date == null || records.length == 0) {
+    res.json({
+      ok: false,
+      message: "wrong parameter",
+    });
+  }
+
+  const data = {
+    id,
+    date,
+    records,
+    createdAt: FieldValue.serverTimestamp(),
+  };
+
+  if (memo != undefined) {
+    data.memo = memo;
+  }
+
+  await db.collection(`users/${uid}/histories`).doc(id).set(data);
+
+  res.json({
+    ok: true,
+  });
+});
--- functions/index.js
+++ functions/index.js
@@ -9,11 +9,11 @@
 
 const {onRequest} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
+const {initializeApp} = require("firebase-admin/app");
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
 
-exports.helloWorld = onRequest((request, response) => {
-  logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+initializeApp()
+
+exports.history = require("./api/history");
