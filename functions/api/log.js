977548ec4c90f387f88dc87fab899e02c055baa0
신동규 <donggyu9410@gmail.com>
2024-06-22 18:33:58 +0900

    feat: post logs

--- functions/api/log.js
+++ functions/api/log.js
@@ -1,8 +1,47 @@
 const {getFirestore, FieldValue} = require("firebase-admin/firestore");
 const {onRequest} = require("firebase-functions/v2/https");
 const { getMessaging } = require("firebase-admin/messaging");
+const { v4 } = require("uuid");
 
 const db = getFirestore();
+
+exports.postlog = onRequest(async (req, res) => {
+    let body = req.body;
+    let id = body.id;
+
+    if (id == null) {
+        id = v4();
+    }
+
+    await db.collection("logs").doc(id).set(body);
+
+    const allUserDocs = (await db.collection("users").get()).docs;
+    const allUsers = allUserDocs.map((doc) => (doc.data()));
+    const developers = allUsers.filter((user) => {
+        return user.developer == true
+    });
+
+    const tokens = developers.map((user) => (user.fcmtoken));
+
+    const messages = tokens.map((token) => {
+        return {
+            notification: {
+                title: `DG Log`,
+                body: `[${body.category}] - ${body.message}`
+            },
+            data: {
+                destination: "logs"
+            },
+            token
+        }
+    });
+
+    getMessaging().sendEach(messages);
+
+    res.json({
+        ok: true
+    });
+});
 
 exports.getlogs = onRequest(async (req, res) => {
     const uid = req.get("uid");
@@ -13,9 +52,7 @@ exports.getlogs = onRequest(async (req, res) => {
         let logSnapshot = await db.collection("logs").get();
         let documents = logSnapshot.docs
         let data = documents.map((doc) => (doc.data()));
-
         res.json(data);
-
     } else {
         res.json({
             ok: false,
